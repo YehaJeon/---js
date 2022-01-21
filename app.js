f8fab18138c58ca9eb3f576770a42314ef62e75d
YehaJeon <aheynoez@gmail.com>
2022-01-21 17:03:46 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+// const loginForm = document.querySelector("#login-form");
+// const loginForm = document.getElementById("login-form");
+
+const loginInput = document.querySelector("#login-form input")
+const loginButton = document.querySelector("#login-form button")
+
+
+function onLoginBtnClick() {
+//    const username = loginInput.value;
+//    if (username === ""){
+//        alert("Please write your name")
+//    }else if ( username.length > 15) {
+//        alert("your name is too young.")
+//    }
+    console.log(username);
+}
+
+
+loginButton.addEventListener("click", onLoginBtnClick);
+
