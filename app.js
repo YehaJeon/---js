e31d1c7bfd899119599c5a1ba6a4cfed4dd15f6e
YehaJeon <aheynoez@gmail.com>
2022-01-21 18:01:41 +0900

    ㅠㅠ

--- app.js
+++ app.js
@@ -1,20 +1,31 @@
-// const loginForm = document.querySelector("#login-form");
-// const loginForm = document.getElementById("login-form");
+// // const loginForm = document.querySelector("#login-form");
+// // const loginForm = document.getElementById("login-form");
 
-const loginInput = document.querySelector("#login-form input")
-const loginButton = document.querySelector("#login-form button")
+// const loginInput = document.querySelector("#login-form input")
+// const loginButton = document.querySelector("#login-form button")
 
 
-function onLoginBtnClick() {
-//    const username = loginInput.value;
-//    if (username === ""){
-//        alert("Please write your name")
-//    }else if ( username.length > 15) {
-//        alert("your name is too young.")
-//    }
-    console.log(username);
+// function onLoginBtnClick() {
+// //    const username = loginInput.value;
+// //    if (username === ""){
+// //        alert("Please write your name")
+// //    }else if ( username.length > 15) {
+// //        alert("your name is too young.")
+// //    }
+//     console.log(username);
+// }
+
+
+// loginButton.addEventListener("click", onLoginBtnClick);
+
+
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+
+function onLoginSubmit(tomato) {
+    tomato.preventDefault();
+    console.log(tomato);
 }
 
-
-loginButton.addEventListener("click", onLoginBtnClick);
+loginForm.addEventListener("submit", onLoginSubmit);
 
