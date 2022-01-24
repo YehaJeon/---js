f4e433e6566a57cf903203dc21569f539dcd7cc4
YehaJeon <aheynoez@gmail.com>
2022-01-24 21:49:34 +0900

    시계

--- JS/greetings.js
+++ JS/greetings.js
@@ -1,6 +1,3 @@
-
-
-
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
@@ -9,7 +6,7 @@ const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY =  "username";
 
 function onLoginSubmit(event) {
-    event.preventDefault(username);
+    event.preventDefault();
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
     localStorage.setItem(USERNAME_KEY, username);
@@ -18,7 +15,7 @@ function onLoginSubmit(event) {
 }
 
 function painGreetings(username) {
-     const username = localStorage.getItem(USERNAME_KEY);
+    //  const username = localStorage.getItem(USERNAME_KEY);
      greeting.innerText = "Hello  " + username;
      greeting.classList.remove(HIDDEN_CLASSNAME);
 }
