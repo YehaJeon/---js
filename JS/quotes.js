2d7412dead3b2e4b0a3684c85c3b9ab41ac671a2
YehaJeon <aheynoez@gmail.com>
2022-01-25 18:06:01 +0900

    quote end

--- JS/quotes.js
+++ JS/quotes.js
@@ -1,61 +1,51 @@
 const quotes = [
     {
-        qutoe: "우린 왜 그냥 스쳐가지 않고 서로를 바라봤을까",
+        quote: "우린 왜 그냥 스쳐가지 않고 서로를 바라봤을까",
         title: "우린 왜 사랑했을까"
     },
     {
-        qutoe: "안녕하세요 우편배달부님! 오늘 오후엔 비가 온대요 우산은 챙기셨나요?",
+        quote: "안녕하세요 우편배달부님! 오늘 오후엔 비가 온대요 우산은 챙기셨나요?",
         title: "나의 방안에"
     },
     {
-        qutoe: "내가 할 수 있는 건 오직 일 분 일 초 매순간 나답게 살아가는 것",
+        quote: "내가 할 수 있는 건 오직 일 분 일 초 매순간 나답게 살아가는 것",
         title: "끝까지 끝은 아니야"
     },
     {
-        qutoe: "일단 정말 출발하고 나면 어쩌면 다 괜찮을지도 몰라",
+        quote: "일단 정말 출발하고 나면 어쩌면 다 괜찮을지도 몰라",
         title: "행운을 빌어줘 Part 2"
     },
     {
-        qutoe: "화분! 방한테 작별 인사했어? 다신 못 볼 테니까 안녕, 해",
+        quote: "화분! 방한테 작별 인사했어? 다신 못 볼 테니까 안녕, 해",
         title: "Goodbye, My Room"
     },
     {
-        qutoe: "밤새 울었던 그 때 내 모습은 너만 아는 비밀로 해줘",
+        quote: "밤새 울었던 그 때 내 모습은 너만 아는 비밀로 해줘",
         title: "Goodbye, My Room"
     },
     {
-        qutoe: "어떤 것에도 불확실은 존재해, 애초 계획은 이게 아니었대도. 괜찮아 ",
+        quote: "어떤 것에도 불확실은 존재해, 애초 계획은 이게 아니었대도. 괜찮아 ",
         title: "행운을 빌어줘 Part 2"
     },
     {
-        qutoe: "그대와 함께하면 어둠도 별빛처럼 평온한 걸 ",
+        quote: "그대와 함께하면 어둠도 별빛처럼 평온한 걸 ",
         title: "낡고 익숙한 사랑 노래"
     },
     {
-        qutoe: "그대는 예상치 못했던 반전 그대로 인해 난 해피엔딩 ",
+        quote: "그대는 예상치 못했던 반전 그대로 인해 난 해피엔딩 ",
         title: "낡고 익숙한 사랑 노래"
     },
     {
-        qutoe: "세상 어떤 사랑도 계절처럼 끝이 와 결국 모든 건 변해 가",
+        quote: "세상 어떤 사랑도 계절처럼 끝이 와 결국 모든 건 변해 가",
         title: "사람들로부터 배운 것"
     }
 
-    
-
-
-    
-
-
-
-
-
-
-
-
-
-]
+];
 
 const quote = document.querySelector("#quote span:first-child");
 const title = document.querySelector("#quote span:last-child");
 
-const todaysQ     
+const todaysQuote = quotes[Math.floor(Math.random()*quotes.length)]
+
+quote.innerText = todaysQuote.quote;
+title.innerText = todaysQuote.title;
