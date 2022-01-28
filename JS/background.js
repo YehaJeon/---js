4d7213d93435fcfc5c01fbd711b2b8bbaf387789
YehaJeon <aheynoez@gmail.com>
2022-01-29 02:48:38 +0900

    ㅋㅋ이게뭐람

--- JS/background.js
+++ JS/background.js
@@ -1,8 +1,9 @@
-const images = ["1.jpg","2.jpg","어햎1.jpg"];
+const images = ["1.jpeg","2.jpeg"];
 
 const chosenImages = images[Math.floor(Math.random()*images.length)];
 
 const bgImage = document.createElement("img");
+bgImage.classList.add('img');
 
 bgImage.src = `img/${chosenImages}`;
 
