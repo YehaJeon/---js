6ce81108ed2b3f0a025a7126ec7ceff53241ebb6
YehaJeon <aheynoez@gmail.com>
2022-01-28 22:35:04 +0900

    The end

--- JS/weather.js
+++ JS/weather.js
@@ -5,9 +5,10 @@ function onGeoOk(position) {
     const lon = position.coords.longitude;  
     const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
     fetch(url).then((response) => response.json()).then(data => {
-    const weatherContainer = document.querySelector("#weather span:first-child")
-    const name = data.name;
-    const weather = data.weather[0].main    
+        const weather = document.querySelector("#weather span:first-child")
+        const city = document.querySelector("#weather span:last-child")
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`
     })
 }
 function onGeoError() {
