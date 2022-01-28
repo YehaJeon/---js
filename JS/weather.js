bb15762123e08e8436e42f57b8177f99028ad1a3
YehaJeon <aheynoez@gmail.com>
2022-01-28 22:03:35 +0900

    weather

--- JS/weather.js
+++ JS/weather.js
@@ -4,7 +4,11 @@ function onGeoOk(position) {
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;  
     const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
-    fetch(url);
+    fetch(url).then((response) => response.json()).then(data => {
+    const weatherContainer = document.querySelector("#weather span:first-child")
+    const name = data.name;
+    const weather = data.weather[0].main    
+    })
 }
 function onGeoError() {
     console.log("can't find you");  
