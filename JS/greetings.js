d0f301f08f0ff9718a2c6da05d5766fac64a638a
YehaJeon <aheynoez@gmail.com>
2022-01-30 01:45:10 +0900

    테스트

--- JS/greetings.js
+++ JS/greetings.js
@@ -17,7 +17,7 @@ function onLoginSubmit(event) {
 
 function painGreetings(username) {
     //  const username = localStorage.getItem(USERNAME_KEY);
-     greeting.innerText = "안녕하세요!  " + username +"님";
+     greeting.innerText = "반가워요!  " + username +"님";
      greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
--- JS/weather.js
+++ JS/weather.js
@@ -7,8 +7,8 @@ function onGeoOk(position) {
     fetch(url).then((response) => response.json()).then(data => {
         const weather = document.querySelector("#weather span:first-child")
         const city = document.querySelector("#weather span:last-child")
-        city.innerText = `  🏵︎ ${data.name}`;
-        weather.innerText = `📻︎${data.weather[0].main}💿︎${data.main.temp} / `
+        city.innerText = `  🌏︎ ${data.name} |`;
+        weather.innerText = `| ☁︎ ${data.weather[0].main} |🌡${data.main.temp} |  `
     })
 }
 function onGeoError() {
