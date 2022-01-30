6432d43d200dc87b4bcfbfac072ec23aed7d770f
YehaJeon <aheynoez@gmail.com>
2022-01-31 00:04:04 +0900

    날씨

--- JS/weather.js
+++ JS/weather.js
@@ -3,7 +3,7 @@ const API_KEY = "ede44ff3564612478f5e2a51a23ebbaf"
 function onGeoOk(position) {
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;  
-    const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
     fetch(url).then((response) => response.json()).then(data => {
         const weather = document.querySelector("#weather span:first-child")
         const city = document.querySelector("#weather span:last-child")
